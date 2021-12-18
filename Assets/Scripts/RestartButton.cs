c126cf68a3d4dec09c6d592ac4dbc88be6e974ec
Ilya Razumov <dangerous-hanter@mail.ru>
2021-12-18 15:27:59 +1000

    Add delay in RestartButton class

--- Assets/Scripts/RestartButton.cs
+++ Assets/Scripts/RestartButton.cs
@@ -7,13 +7,25 @@ public class RestartButton : MonoBehaviour
     [SerializeField]
     public GameStarter GameStarter;
 
+    bool _isButtonShowing = false;
+
     public void ShowRestartButton()
     {
-        gameObject.SetActive(true);
+        Invoke("SetButtonActive", 0.1f);
+    }
+
+    void SetButtonActive()
+    {
+        if (!_isButtonShowing)
+        {
+            gameObject.SetActive(true);
+            _isButtonShowing = true;
+        }
     }
 
     public void HideRestartButton()
     {
         gameObject.SetActive(false);
+        _isButtonShowing = false;
     }
 }
