a58a6ed8edf27b31c1143746660d471b9009e590
Ilya Razumov <dangerous-hanter@mail.ru>
2021-12-18 17:22:00 +1000

    Add Cell animations

--- Assets/Scripts/Cell.cs
+++ Assets/Scripts/Cell.cs
@@ -2,19 +2,28 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using DG.Tweening;
 
 public class Cell : MonoBehaviour
 {
     [SerializeField]
-    public GameObject SpriteRenderer;
+    public GameObject CardSprite;
 
     int _identifier;
     float _rotationAngle;
     Sprite _cellSprite;
+    Vector3 _cellSize;
+    Vector3 _cardSpriteSize;
+
+    void Awake()
+    {
+        _cellSize = Vector3.Scale(gameObject.GetComponent<BoxCollider2D>().size, gameObject.GetComponent<Transform>().localScale);
+        _cardSpriteSize = Vector3.Scale(_cellSize, CardSprite.GetComponent<Transform>().localScale);
+    }
 
     void OnMouseDown()
     {
-        GameObject.Find("CardChecker").GetComponent<CardChecker>().CompareCardIdentifiers(_identifier);
+        GameObject.Find("CardChecker").GetComponent<CardChecker>().CompareCardIdentifiers(_identifier, this);
     }
 
     public void SetCellParameters(int identifier, Sprite sprite, float rotationAngle)
@@ -27,8 +36,26 @@ public class Cell : MonoBehaviour
 
     void VisualizeSprite()
     {
-        SpriteRenderer.GetComponent<SpriteRenderer>().sprite = _cellSprite;
-        SpriteRenderer.transform.rotation = Quaternion.identity;
-        SpriteRenderer.transform.Rotate(new Vector3(0, 0, -_rotationAngle));
+        CardSprite.GetComponent<SpriteRenderer>().sprite = _cellSprite;
+        CardSprite.transform.rotation = Quaternion.identity;
+        CardSprite.transform.Rotate(new Vector3(0, 0, -_rotationAngle));
+    }
+
+    public void DoBounce(float time)
+    {
+        gameObject.transform.DORewind();
+        gameObject.transform.DOShakeScale(time, strength: _cellSize / 4, vibrato: 8, randomness: 1);
+    }
+
+    public void DoBounceCard(float time)
+    {
+        gameObject.transform.GetChild(0).gameObject.transform.DORewind();
+        gameObject.transform.GetChild(0).gameObject.transform.DOShakeScale(time, strength: _cardSpriteSize / 4, vibrato: 6, randomness: 1);
+    }
+
+    public void DoShakeCard(float time)
+    {
+        gameObject.transform.GetChild(0).gameObject.transform.DORewind();
+        gameObject.transform.GetChild(0).gameObject.transform.DOShakePosition(time, strength: _cardSpriteSize / 3, vibrato: 14, randomness: 60);
     }
 }
