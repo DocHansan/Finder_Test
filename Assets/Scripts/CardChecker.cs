bc12abf76d1c0c93a1173927cd3ae58ab5f71e42
Ilya Razumov <dangerous-hanter@mail.ru>
2021-12-18 17:23:17 +1000

    Add call cell animation in CardChecker

--- Assets/Scripts/CardChecker.cs
+++ Assets/Scripts/CardChecker.cs
@@ -6,15 +6,34 @@ public class CardChecker : MonoBehaviour
 {
     [SerializeField]
     public GameField GameField;
+    [SerializeField]
+    public GameObject Cell;
 
-    public void CompareCardIdentifiers(int klickedCellIdentifier)
+    float _bounceCardAnimTime = 0.5f;
+    float _shakeCardAnimTime = 0.5f;
+    bool _isRightCardCklicked = false;
+
+    public void CompareCardIdentifiers(int klickedCellIdentifier, Cell cell)
     {
         if (klickedCellIdentifier == GameField.GetCardIdentifier())
-            ChangeLevel();
+        {
+            cell.DoBounceCard(_bounceCardAnimTime);
+            if (!_isRightCardCklicked)
+            {
+                _isRightCardCklicked = true;
+                Invoke("ChangeLevel", _bounceCardAnimTime);
+            }
+            
+        }
+        else
+        {
+            cell.DoShakeCard(_shakeCardAnimTime);
+        }
     }
 
     void ChangeLevel()
     {
+        _isRightCardCklicked = false;
         GameField.ChangeLevel—omplexity();
     }
 }
