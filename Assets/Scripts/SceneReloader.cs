df68e9b2fa8c7c154cb21f019fdbdd0c68db64f4
Ilya Razumov <dangerous-hanter@mail.ru>
2021-12-19 12:16:54 +1000

    Add SceneReloader class

---
+++ Assets/Scripts/SceneReloader.cs
@@ -0,0 +1,53 @@
+using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class SceneReloader : MonoBehaviour
+{
+    [SerializeField]
+    public Text LoadProgress;
+
+    FadeAnimator _fadeAnimator;
+    Image _endScreenImage;
+    float _fadeTime = 1f;
+    float _loadTime = 1f;
+
+    void Awake()
+    {
+        _fadeAnimator = GetComponent<FadeAnimator>();
+        _endScreenImage = GetComponent<Image>();
+    }
+
+    public void RestartScene()
+    {
+        StartCoroutine(MakeFadeEndScreen());
+    }
+
+    IEnumerator MakeFadeEndScreen()
+    {
+        _fadeAnimator.Fade(_fadeTime, _endScreenImage, true);
+        yield return new WaitForSeconds(_fadeTime);
+        StartCoroutine(UpdateLoadProgress());
+        
+    }
+
+    IEnumerator UpdateLoadProgress()
+    {
+        float passedTime = 0f;
+        while (passedTime < _loadTime)
+        {
+            passedTime += Time.deltaTime / _loadTime;
+            LoadProgress.text = Mathf.Round(passedTime / _loadTime * 100) + "%";
+            yield return null;
+        }
+        LoadProgress.text = "";
+        StartCoroutine(ReloadScene());
+    }
+
+    IEnumerator ReloadScene()
+    {
+        SceneManager.LoadSceneAsync(0);
+        yield return null;
+    }
+}
